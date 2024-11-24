3cea61a4e94eafeba30c66c610ac0fffbe2bfb23
Fathir Amanda <fathirrandhanpratama@gmail.com>
2024-11-24 11:53:00 +0700

    Add files via upload

---
+++ sshws/insshws.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# System Request : Debian 9+/Ubuntu 18.04+/20+
+# Develovers » Riswanvpn
+# Email      » Riswanvpn@gmail.com
+# telegram   » https://t.me/Riswanvpn
+
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Riswanvpn
+
+cd
+
+#Install Script Websocket-SSH Python
+#wget -O /usr/local/bin/ws-openssh https://dpvpn.me/sc/sshws/openssh-socket.py
+wget -O /usr/local/bin/ws-dropbear https://dpvpn.me/sc/sshws/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://dpvpn.me/sc/sshws/ws-stunnel
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/OPENVPN/ws-ovpn
+
+#izin permision
+#chmod +x /usr/local/bin/ws-openssh
+chmod +x /usr/local/bin/ws-dropbear
+chmod +x /usr/local/bin/ws-stunnel
+chmod +x /usr/local/bin/ws-ovpn
+
+
+#System OpenSSH Websocket-SSH Python
+#wget -O /etc/systemd/system/ws-openssh.service https://dpvpn.me/sc/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
+
+#System Dropbear Websocket-SSH Python
+wget -O /etc/systemd/system/ws-dropbear.service https://dpvpn.me/sc/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+
+#System SSL/TLS Websocket-SSH Python
+wget -O /etc/systemd/system/ws-stunnel.service https://dpvpn.me/sc/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+
+##System Websocket-OpenVPN Python
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/OPENVPN/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+
+#restart service
+#
+systemctl daemon-reload
+#Enable & Start & Restart ws-openssh service
+#systemctl enable ws-openssh.service
+#systemctl start ws-openssh.service
+#systemctl restart ws-openssh.service
+
+#Enable & Start & Restart ws-dropbear service
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+
+#Enable & Start & Restart ws-openssh service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
+
+#Enable & Start ws-ovpn service
+systemctl enable ws-ovpn.service
+systemctl start ws-ovpn.service
+systemctl restart ws-ovpn.service
