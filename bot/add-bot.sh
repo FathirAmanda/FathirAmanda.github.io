eefbb42253a570ef44dd395efd1ea0d83baabb5f
Fathir Amanda <fathirrandhanpratama@gmail.com>
2024-11-24 23:20:41 +0700

    Add files via upload

--- bot/add-bot.sh
+++ bot/add-bot.sh
@@ -19,13 +19,13 @@ apt update && apt upgrade
 apt install neofetch -y
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://dpvpn.me/sc/botol/bot.zip
+wget https://dpvpn.me/botol/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://dpvpn.me/sc/botol/kyt.zip
+wget https://dpvpn.me/botol/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 clear
@@ -69,79 +69,79 @@ echo -e " \033[1;97;41m     MENDOWNLOAD ASSET TAMBAHAN......    \033[0m"
 
 
 
-wget -q -O /usr/bin/addnoobz "https://dpvpn.me/sc/botol/addnoobz.sh" && chmod +x /usr/bin/addnoobz
+wget -q -O /usr/bin/addnoobz "https://dpvpn.me/botol/addnoobz.sh" && chmod +x /usr/bin/addnoobz
 
 
 
 
 
 
-wget -q -O /usr/bin/add-vless "https://dpvpn.me/sc/botol/add-vless.sh" && chmod +x /usr/bin/add-vless
+wget -q -O /usr/bin/add-vless "https://dpvpn.me/botol/add-vless.sh" && chmod +x /usr/bin/add-vless
 
-wget -q -O /usr/bin/addtr "https://dpvpn.me/sc/botol/addtr.sh" && chmod +x /usr/bin/addtr
-
-
-wget -q -O /usr/bin/addws "https://dpvpn.me/sc/botol/addws.sh" && chmod +x /usr/bin/addws
+wget -q -O /usr/bin/addtr "https://dpvpn.me/botol/addtr.sh" && chmod +x /usr/bin/addtr
 
 
-wget -q -O /usr/bin/addss "https://dpvpn.me/sc/botol/addss.sh" && chmod +x /usr/bin/addss
-
-wget -q -O /usr/bin/cek-ssh "https://dpvpn.me/sc/botol/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
+wget -q -O /usr/bin/addws "https://dpvpn.me/botol/addws.sh" && chmod +x /usr/bin/addws
 
 
+wget -q -O /usr/bin/addss "https://dpvpn.me/botol/addss.sh" && chmod +x /usr/bin/addss
 
-wget -q -O /usr/bin/cek-ss "https://dpvpn.me/sc/botol/cek-ss.sh" && chmod +x /usr/bin/cek-ss
+wget -q -O /usr/bin/cek-ssh "https://dpvpn.me/botol/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
 
 
 
-
-wget -q -O /usr/bin/cek-tr "https://dpvpn.me/sc/botol/cek-tr.sh" && chmod +x /usr/bin/cek-tr
-
-wget -q -O /usr/bin/cek-vless "https://dpvpn.me/sc/botol/cek-vless.sh" && chmod +x /usr/bin/cek-vless
-
-wget -q -O /usr/bin/cek-ws "https://dpvpn.me/sc/botol/cek-ws.sh" && chmod +x /usr/bin/cek-ws
-
-wget -q -O /usr/bin/del-vless "https://dpvpn.me/sc/botol/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -q -O /usr/bin/cek-ss "https://dpvpn.me/botol/cek-ss.sh" && chmod +x /usr/bin/cek-ss
 
 
 
 
-wget -q -O /usr/bin/cek-noobz "https://dpvpn.me/sc/botol/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
+wget -q -O /usr/bin/cek-tr "https://dpvpn.me/botol/cek-tr.sh" && chmod +x /usr/bin/cek-tr
+
+wget -q -O /usr/bin/cek-vless "https://dpvpn.me/botol/cek-vless.sh" && chmod +x /usr/bin/cek-vless
+
+wget -q -O /usr/bin/cek-ws "https://dpvpn.me/botol/cek-ws.sh" && chmod +x /usr/bin/cek-ws
+
+wget -q -O /usr/bin/del-vless "https://dpvpn.me/botol/del-vless.sh" && chmod +x /usr/bin/del-vless
 
 
 
-wget -q -O /usr/bin/deltr "https://dpvpn.me/sc/botol/deltr.sh" && chmod +x /usr/bin/deltr
 
-wget -q -O /usr/bin/delws "https://dpvpn.me/sc/botol/delws.sh" && chmod +x /usr/bin/delws
-
-
-
-wget -q -O /usr/bin/delss "https://dpvpn.me/sc/botol/delss.sh" && chmod +x /usr/bin/delss
+wget -q -O /usr/bin/cek-noobz "https://dpvpn.me/botol/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
 
 
 
-wget -q -O /usr/bin/renew-ss "https://dpvpn.me/sc/botol/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+wget -q -O /usr/bin/deltr "https://dpvpn.me/botol/deltr.sh" && chmod +x /usr/bin/deltr
 
-
-wget -q -O /usr/bin/renewtr "https://dpvpn.me/sc/botol/renewtr.sh" && chmod +x /usr/bin/renewtr
-
-
-wget -q -O /usr/bin/renewvless "https://dpvpn.me/sc/botol/renewvless.sh" && chmod +x /usr/bin/renewvless
-
-
-wget -q -O /usr/bin/renewws "https://dpvpn.me/sc/botol/renewws.sh" && chmod +x /usr/bin/renewws
-
-
-wget -q -O /usr/bin/cek-mws "https://dpvpn.me/sc/botol/cek-mws.sh" && chmod +x /usr/bin/cek-mws
-
-wget -q -O /usr/bin/cek-mvs "https://dpvpn.me/sc/botol/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
-
-
-wget -q -O /usr/bin/cek-mss "https://dpvpn.me/sc/botol/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+wget -q -O /usr/bin/delws "https://dpvpn.me/botol/delws.sh" && chmod +x /usr/bin/delws
 
 
 
-wget -q -O /usr/bin/cek-mts "https://dpvpn.me/sc/botol/cek-mts.sh" && chmod +x /usr/bin/cek-mts
+wget -q -O /usr/bin/delss "https://dpvpn.me/botol/delss.sh" && chmod +x /usr/bin/delss
+
+
+
+wget -q -O /usr/bin/renew-ss "https://dpvpn.me/botol/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+
+
+wget -q -O /usr/bin/renewtr "https://dpvpn.me/botol/renewtr.sh" && chmod +x /usr/bin/renewtr
+
+
+wget -q -O /usr/bin/renewvless "https://dpvpn.me/botol/renewvless.sh" && chmod +x /usr/bin/renewvless
+
+
+wget -q -O /usr/bin/renewws "https://dpvpn.me/botol/renewws.sh" && chmod +x /usr/bin/renewws
+
+
+wget -q -O /usr/bin/cek-mws "https://dpvpn.me/botol/cek-mws.sh" && chmod +x /usr/bin/cek-mws
+
+wget -q -O /usr/bin/cek-mvs "https://dpvpn.me/botol/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
+
+
+wget -q -O /usr/bin/cek-mss "https://dpvpn.me/botol/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+
+
+
+wget -q -O /usr/bin/cek-mts "https://dpvpn.me/botol/cek-mts.sh" && chmod +x /usr/bin/cek-mts
 
 cd /root
 rm -rf kyt.sh
